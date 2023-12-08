b58b26d6d03672a8998032268ebc447649bb256c
Marcin Niemirski <marcin.niemirski@tsh.io>
2023-12-08 15:52:06 +0100

    feat: add plop for api actions

--- plopfile.js
+++ plopfile.js
@@ -148,8 +148,59 @@ const reactContextGenerator = () => ({
   },
 });
 
+const apiActionsGenerator = () => ({
+  description: componentTypes.API_ACTIONS,
+  prompts: [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'actions collection name',
+      validate: (input) => input.length > 1 || 'Actions collection name cannot be empty!',
+    },
+  ],
+  actions: function () {
+    return [
+      {
+        type: 'add',
+        path: 'src/api/actions/{{camelCase name}}/{{camelCase name}}.mutations.ts',
+        templateFile: 'plop-templates/apiActions/apiActions.mutations.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/api/actions/{{camelCase name}}/{{camelCase name}}.queries.ts',
+        templateFile: 'plop-templates/apiActions/apiActions.queries.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/api/actions/{{camelCase name}}/{{camelCase name}}.types.ts',
+        templateFile: 'plop-templates/apiActions/apiActions.types.hbs',
+      },
+      {
+        type: 'modify',
+        path: 'src/api/actions/index.ts',
+        pattern: getPlaceholderPattern('API_COLLECTION_IMPORTS'),
+        template:
+          "import { {{camelCase name}}Mutations } from './{{camelCase name}}/{{camelCase name}}.mutations';\nimport { {{camelCase name}}Queries } from './{{camelCase name}}/{{camelCase name}}.queries';\n$1",
+      },
+      {
+        type: 'modify',
+        path: 'src/api/actions/index.ts',
+        pattern: getPlaceholderPattern('API_COLLECTION_QUERIES'),
+        template: '...{{camelCase name}}Queries,\n  $1',
+      },
+      {
+        type: 'modify',
+        path: 'src/api/actions/index.ts',
+        pattern: getPlaceholderPattern('API_COLLECTION_MUTATIONS'),
+        template: '...{{camelCase name}}Mutations,\n  $1',
+      },
+    ];
+  },
+});
+
 module.exports = function (plop) {
   plop.setGenerator(componentTypes.COMPONENT, componentGenerator());
   plop.setGenerator(componentTypes.SCREEN, screenGenerator());
   plop.setGenerator(componentTypes.REACT_CONTEXT, reactContextGenerator());
+  plop.setGenerator(componentTypes.API_ACTIONS, apiActionsGenerator());
 };
