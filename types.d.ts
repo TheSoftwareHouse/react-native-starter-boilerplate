fad845c6aacdbc0fa7c27d0b8c7de671dee751db
Daniel Markiel <daniel.markiel@tsh.io>
2024-09-09 10:13:49 +0200

    chore: Upgrade react native to v0.75

--- types.d.ts
+++ types.d.ts
@@ -2,7 +2,6 @@ import { RootStackParamList } from './src/navigations/Navigator.types';
 
 declare global {
   namespace ReactNavigation {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
     interface RootParamList extends RootStackParamList {}
   }
 }
